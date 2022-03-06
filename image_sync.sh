7f86de497a494f79fcbeea435fbe92ded6d13d43
acejilam <acejilam@gmail.com>
2022-03-06 16:40:11 +0800

    Update image_sync.sh

--- image_sync.sh
+++ image_sync.sh
@@ -1,53 +1,35 @@
-docker login -u acejilam
-
+#!/bin/bash
 cat >sync_image.sh <<\EOF
-# set -x xtrace
-# export PS4='[Line:${LINENO}] '
+#set -x xtrace
+#export PS4='[Line:${LINENO}] '
 sync() {
-    address=$1
-    arr=(${address//\// })
-    Repo=${arr[${#arr[@]}-1]}
+  address=$1
+  # shellcheck disable=SC2206
+  arr=(${address//\// })
+  Repo=${arr[${#arr[@]} - 1]}
+  echo $Repo1
+  echo gcloud container images list-tags $address
 
-    for line in $(gcloud container images list-tags $address | grep -v TAGS | awk '{printf("%s@%s\n", $1,$2)}'); do
-        _id=$(echo $line | awk -F'@' '{print $1}')
-        tag_date=$(echo $line | awk -F'@' '{print $2}')
-        _date=$(echo $tag_date | awk -F'T' '{print $1}')
-        _time=$(echo $tag_date | awk -F'T' '{print $2}')
+  address=k8s.gcr.io/kube-state-metrics/kube-state-metrics
+  for tag in $(gcloud container images list-tags $address); do
+    if [[ "$tag" == "TAGS:" ]] || [[ "$tag" == "DIGEST:" ]]; then
+      continue
+    else
 
-        if echo $_date | grep -q '\<[0-9]\{4\}-[0-9]\{1,2\}-[0-9]\{1,2\}\>'; then
-            continue
-        else
-            tag=$tag_date
-            flag=false
+      if [[ "$tag" =~ [0-9a-zA-Z]{12}$ ]]; then
+        continue
+      else
+        echo -e "\033[34mpulling  $address:$tag\033[0m"
+        docker pull $address:$tag
+        docker tag $address:$tag acejilam/$Repo:$tag
+        docker push acejilam/$Repo:$tag
+        echo -e "\033[32msync  acejilam/$Repo:$tag\033[0m"
+        docker rmi acejilam/$Repo:$tag
+        docker rmi $address:$tag
+      fi
+    fi
 
-            curl -o /tmp/res.txt -s https://hub.docker.com/v2/repositories/acejilam/$Repo/tags/?page_size=1000&page=1
-            res=`cat /tmp/res.txt |jq -r '.results[].name'`
-            array=(${res// /,})
-            len=${#array[*]}
-            for item in $res; do
-                if [ "$item" == "$tag" ]; then
-                    flag=true
-                    echo -e "\033[32m存在  acejilam/$Repo:$tag\033[0m"
-                    break
-                fi
-            done
-            if [ "$tag" == "latest" ]; then
-                flag=false
-            fi
-            echo $len ' ------->' $flag $Repo:$tag
-            if $flag; then
-                continue
-            else
-                echo -e "\033[34mpulling  $address:$tag\033[0m"
-                docker pull $address:$tag
-                docker tag $address:$tag acejilam/$Repo:$tag
-                docker push acejilam/$Repo:$tag
-                echo -e "\033[32msync  acejilam/$Repo:$tag\033[0m"
-                docker rmi acejilam/$Repo:$tag
-                docker rmi $address:$tag
-            fi
-        fi
-    done
+  done
 }
 sync 'k8s.gcr.io/kube-state-metrics/kube-state-metrics'
 sync 'k8s.gcr.io/ingress-nginx/controller'
@@ -64,4 +46,4 @@ sync 'k8s.gcr.io/sig-storage/csi-node-driver-registrar'
 sync 'k8s.gcr.io/sig-storage/csi-resizer'
 EOF
 chmod +x sync_image.sh
-./sync_image.sh
+bash sync_image.sh
