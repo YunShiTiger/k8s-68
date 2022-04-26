48b96b4562baabb04103a3e98d4e6bc54a8470d7
acejilam <acejilam@gmail.com>
2022-04-26 10:40:51 +0800

    fix

--- efk/build_image.sh
+++ efk/build_image.sh
@@ -1,7 +1,7 @@
 # shellcheck disable=SC2164
 #cd log-gen
-#docker build -t harbor.vackbot.com/liexing/log-gen:v1 .
-#docker push harbor.vackbot.com/liexing/log-gen:v1
+#docker build -t harbor.ls.com/lie/log-gen:v1 .
+#docker push harbor.ls.com/lie/log-gen:v1
 #cd ..
 #kubectl -n mesoid create configmap filebeat --from-file=conf=./filebeat/filebeat.yml
 kubectl apply -f efk.yaml
