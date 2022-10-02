af8b89512a9b696671ca5623a36359311b4b3854
刘硕 <liushuo@vackbot.com>
2022-10-02 21:58:44 +0800

    add es

--- es/run.sh
+++ es/run.sh
@@ -1,17 +1,16 @@
 # https://www.cnblogs.com/evescn/p/16340453.html
 
-
+mkdir -p /root/elk/es/certs
+cd /root/elk/es/certs
 wget https://get.helm.sh/helm-v3.10.0-linux-amd64.tar.gz
 tar -zxvf helm-v3.10.0-linux-amd64.tar.gz
 mv linux-amd64/helm /usr/bin/
 rm -rf linux-amd64/
 
-
-
 docker rm -f elastic-charts-certs
 
 # 运行容器生成证书
-docker run --name elastic-charts-certs -i -w /app elasticsearch:7.16.3 /bin/sh -c  \
+docker run --name elastic-charts-certs -i -w /app elasticsearch:7.16.3 /bin/sh -c \
   "elasticsearch-certutil ca --out /app/elastic-stack-ca.p12 --pass '' && \
     elasticsearch-certutil cert --ca /app/elastic-stack-ca.p12 --pass '' --ca-pass '' --out /app/elastic-certificates.p12"
 
@@ -24,7 +23,7 @@ docker rm -f elastic-charts-certs
 # 将 pcks12 中的信息分离出来，写入文件
 openssl pkcs12 -nodes -passin pass:'' -in elastic-certificates.p12 -out elastic-certificate.pem
 
-kubectl delete secret  elastic-certificates elastic-certificate-pem elastic-credentials
+kubectl delete secret elastic-certificates elastic-certificate-pem elastic-credentials
 
 # 创建 test-middleware 名称空间
 kubectl create ns test-middleware
@@ -37,9 +36,11 @@ kubectl -n test-middleware create secret generic elastic-certificate-pem --from-
 kubectl -n test-middleware create secret generic elastic-credentials --from-literal=username=elastic --from-literal=password=admin@123
 kubectl -n test-middleware get secret
 
+cd /root/elk/es/
 # 添加 Chart 仓库
-helm repo add  elastic    https://helm.elastic.co
+helm repo add elastic https://helm.elastic.co
 helm repo update
+# 拉取 chart 到本地 /root/elk/es 目录
 helm pull elastic/elasticsearch --version 7.16.3
 tar -zxvf elasticsearch-7.16.3.tgz
 cp elasticsearch/values.yaml ./values-test.yaml
@@ -48,22 +49,21 @@ cp elasticsearch/values.yaml ./values-test.yaml
 helm -n test-middleware install elasticsearch-master -f es-master-values.yaml --version 7.16.3 elastic/elasticsearch
 
 # 安装 ElasticSearch Data 节点
-helm install elasticsearch-data -f es-data-values.yaml --version 7.16.3 elastic/elasticsearch
+helm -n test-middleware install elasticsearch-data -f es-data-values.yaml --version 7.16.3 elastic/elasticsearch
 
 # 安装 ElasticSearch Client 节点
-helm install elasticsearch-client -f es-client-values.yaml --version 7.16.3 elastic/elasticsearch
+helm -n test-middleware install elasticsearch-client -f es-client-values.yaml --version 7.16.3 elastic/elasticsearch
 
 # 安装 kibana 节点
-helm install kibana -f es-kibana-values.yaml --version 7.16.3 elastic/kibana
+helm -n test-middleware install kibana -f es-kibana-values.yaml --version 7.16.3 elastic/kibana
 
-# kubectl get pods --namespace=default -l app=elasticsearch-master -w
+kubectl get pods --namespace=test-middleware -l app=elasticsearch-master -w
 # helm test elasticsearch-master --cleanup
 
-helm uninstall elasticsearch-client
-helm uninstall elasticsearch-data
-helm uninstall elasticsearch-master
-helm uninstall kibana
-
+helm uninstall -n test-middleware kibana
+helm uninstall -n test-middleware elasticsearch-client
+helm uninstall -n test-middleware elasticsearch-data
+helm uninstall -n test-middleware elasticsearch-master
 
 #http://k8s-worker-1:30601
 #elastic/admin@123
